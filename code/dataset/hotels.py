a771160e1824cfa116277e932b2ad64b85766fd2
Aarash Feizi <aarash95@gmail.com>
2021-08-17 19:18:06 -0400

    fix hotels loader

--- code/dataset/hotels.py
+++ code/dataset/hotels.py
@@ -1,11 +1,11 @@
 from .base import *
-
+from tqdm import tqdm
 
 class Hotels(BaseDataset):
     def __init__(self, root, mode, transform=None):
         self.mode = mode
         if mode == 'train':
-            self.root = root + '/hotels50k_v5_restructured/train'
+            self.root = root + '/hotels50k_v5_restructured/train_small'
         elif self.mode == 'eval':
             self.root = root + '/hotels50k_v5_restructured/val1_small'
         self.transform = transform
@@ -17,13 +17,16 @@ class Hotels(BaseDataset):
 
         BaseDataset.__init__(self, self.root, self.mode, self.transform)
         index = 0
-        for i in torchvision.datasets.ImageFolder(root=self.root).imgs:
-            # i[1]: label, i[0]: root
-            y = i[1]
-            # fn needed for removing non-images starting with `._`
-            fn = os.path.split(i[0])[1]
-            if y in self.classes and fn[:2] != '._':
-                self.ys += [y]
-                self.I += [index]
-                self.im_paths.append(os.path.join(self.root, i[0]))
-                index += 1
+        with tqdm(total=len(torchvision.datasets.ImageFolder(root=self.root).imgs), desc=f'Loading hotels {mode}...') as t:
+            for i in torchvision.datasets.ImageFolder(root=self.root).imgs:
+                # i[1]: label, i[0]: root
+                y = i[0].split('/')[-2]
+                # fn needed for removing non-images starting with `._`
+                fn = os.path.split(i[0])[1]
+                if y in self.classes and fn[:2] != '._':
+                    self.ys += [y]
+                    self.I += [index]
+                    self.im_paths.append(os.path.join(self.root, i[0]))
+                    index += 1
+
+            t.update()
